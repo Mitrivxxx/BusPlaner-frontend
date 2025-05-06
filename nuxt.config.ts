b71b5319024b8de9ec9ec6ffdea6abd64757bedd
Mateusz <matsma2468@gmail.com>
2025-05-06 11:44:30 +0200

    łączenie z fast api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.API_BASE_URL || 'http://localhost:8000'
+    }
+  },
+
+  compatibilityDate: '2025-05-06'
 })
---
+++ pages/test.vue
@@ -0,0 +1,22 @@
+<script setup>
+const message = ref("Kliknij przycisk, aby połączyć się z FastAPI")
+
+const fetchData = async () => {
+  try {
+    const response = await fetch('http://localhost:8000/')
+    const data = await response.json()
+    message.value = data.message
+  } catch (error) {
+    message.value = "Błąd połączenia z backendem"
+    console.error(error)
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Połączenie Nuxt.js 3 z FastAPI</h1>
+    <p>{{ message }}</p>
+    <button @click="fetchData">Pobierz dane</button>
+  </div>
+</template>
